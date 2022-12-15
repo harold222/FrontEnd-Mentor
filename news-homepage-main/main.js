3d588d23c0e206554c0064fac943318bed198e30
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-12-15 16:31:48 -0500

    menu responsive

---
+++ news-homepage-main/main.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    document.getElementById('check').addEventListener('change', (e) => {
+        const hiddeNav = existNav();
+        if (e.target.checked && hiddeNav) {
+            const rear = document.createElement("div");
+            rear.classList.add('rear-container');
+            document.body.appendChild(rear);
+            hiddeNav.classList.add('show-header-navigation-menu');
+        }
+    });
+
+    document.getElementById('close').addEventListener('change', (e) => {
+        const rearContainer = document.getElementsByClassName('rear-container');
+        const hiddeNav = existNav();
+        hiddeNav.classList.remove('show-header-navigation-menu');
+        document.body.removeChild(rearContainer[0]);
+    });
+
+    const existNav = () => {
+        const hiddeNav = document.getElementsByClassName('header-navigation-menu');
+        return hiddeNav?.length > 0 ? hiddeNav[0] : null;
+    }
+
+})();
